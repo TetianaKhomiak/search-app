95471d1da52eaa39229eee2dc274dbd19fd48242
Tetiana Khomiak <tkhomiak8889@gmail.com>
2024-01-15 20:34:55 +0100

    add .

--- app.js
+++ app.js
@@ -235,25 +235,40 @@ class UI {
     this.alertContainer = document.querySelector(".search");
   }
 
+  formatDate(date) {
+    const formattedDate = new Date(date).toLocaleDateString("en-GB");
+    return formattedDate;
+  }
+
   showProfile(user) {
     this.profile.innerHTML = `
       <div class="card card-body mb-3">
         <div class="row">
           <div class="col-md-3">
             <img class="img-fluid mb-2" src="${user.avatar_url}">
-            <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-4">View Profile</a>
+            <a href="${
+              user.html_url
+            }" target="_blank" class="btn btn-primary btn-block mb-4">View Profile</a>
           </div>
           <div class="col-md-9">
-            <span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
-            <span class="badge badge-secondary">Public Gists: ${user.public_gists}</span>
-            <span class="badge badge-success">Followers: ${user.followers}</span>
+            <span class="badge badge-primary">Public Repos: ${
+              user.public_repos
+            }</span>
+            <span class="badge badge-secondary">Public Gists: ${
+              user.public_gists
+            }</span>
+            <span class="badge badge-success">Followers: ${
+              user.followers
+            }</span>
             <span class="badge badge-info">Following: ${user.following}</span>
             <br><br>
             <ul class="list-group">
               <li class="list-group-item">Company: ${user.company}</li>
               <li class="list-group-item">Website/Blog: ${user.blog}</li>
               <li class="list-group-item">Location: ${user.location}</li>
-              <li class="list-group-item">Member Since: ${user.created_at}</li>
+              <li class="list-group-item">Member Since: ${this.formatDate(
+                user.created_at
+              )}</li>
             </ul>
           </div>
         </div>
@@ -262,6 +277,7 @@ class UI {
       <div class="repos"></div>
     `;
   }
+
   showRepos(repos) {
     this.profile.insertAdjacentHTML(
       "beforeend",
