b0bd2c7d4ffe46f51e4f2b2de2e5501a9374ec05
Tetiana Khomiak <tkhomiak8889@gmail.com>
2024-01-15 20:11:39 +0100

    add .

--- app.js
+++ app.js
@@ -186,8 +186,10 @@ class GitHubController {
         }, 500);
         return;
       } else if (userData.html_url) {
+        this.ui.showProfile(userData);
         const userRepos = await this.gitHubServiceRepos.getRepos(inputValue);
-        return this.ui.showProfile(userData, userRepos);
+
+        return this.ui.showRepos(userRepos);
       }
     }
 
@@ -233,26 +235,18 @@ class UI {
     this.alertContainer = document.querySelector(".search");
   }
 
-  showProfile(user, repos) {
+  showProfile(user) {
     this.profile.innerHTML = `
       <div class="card card-body mb-3">
         <div class="row">
           <div class="col-md-3">
             <img class="img-fluid mb-2" src="${user.avatar_url}">
-            <a href="${
-              user.html_url
-            }" target="_blank" class="btn btn-primary btn-block mb-4">View Profile</a>
+            <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-4">View Profile</a>
           </div>
           <div class="col-md-9">
-            <span class="badge badge-primary">Public Repos: ${
-              user.public_repos
-            }</span>
-            <span class="badge badge-secondary">Public Gists: ${
-              user.public_gists
-            }</span>
-            <span class="badge badge-success">Followers: ${
-              user.followers
-            }</span>
+            <span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
+            <span class="badge badge-secondary">Public Gists: ${user.public_gists}</span>
+            <span class="badge badge-success">Followers: ${user.followers}</span>
             <span class="badge badge-info">Following: ${user.following}</span>
             <br><br>
             <ul class="list-group">
@@ -265,16 +259,20 @@ class UI {
         </div>
       </div>
       <h3 class="page-heading mb-3">Latest Repos</h3>
-      <div class="repos">
-      ${repos
+      <div class="repos"></div>
+    `;
+  }
+  showRepos(repos) {
+    this.profile.insertAdjacentHTML(
+      "beforeend",
+      ` ${repos
         .slice(0, 5)
         .map(
           (repo) =>
             `<p>${repo.name}: <a href="${repo.html_url}" target="_blank">${repo.html_url}</a></p>`
         )
-        .join("")}
-    </div>
-    `;
+        .join("")}`
+    );
   }
 
   clearProfile() {
